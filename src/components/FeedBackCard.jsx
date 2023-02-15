8692a97145f389c9048c9dd7ab5c7ec97c1e2a41
Alaminxab <alaminxab@gmail.com>
2023-02-15 14:14:13 +0000

    more design

--- src/components/FeedBackCard.jsx
+++ src/components/FeedBackCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { quotes } from '../assets'
 
-const FeedBackCard = () => {
-  return (
-    <div>FeedBackCard</div>
+const FeedBackCard = ({content, name, title, image}) =>(
+    <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card'>
+      <img src={quotes} className='w-[42px] h-[27px] object-contain'/>
+    </div>
   )
-}
 
 export default FeedBackCard
